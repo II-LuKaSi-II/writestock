7b480b817ba211fc296a4d03ef718a3ad7dddbb8
Lucas Bennett <luc.bennett@gmail.com>
2020-11-21 16:13:20 -0800

    Formatting finish

--- routes/home.js
+++ routes/home.js
@@ -6,6 +6,9 @@ const { ensureAuthenticated } = require('../config/auth')
 //Welcome Home Page
 router.get('/', (req, res) => res.render('welcome'))
 
+router.get('/about', (req, res) => res.render('about'))
+
+router.get('/contact', (req, res) => res.render('contact'))
 //Dashboard page rendering dashbaord view
 //pass in authentification function as second parameter to protect this route
 router.get('/dashboard', ensureAuthenticated, (req, res) => 
